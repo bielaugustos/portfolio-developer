ac15b34e906262d2e9091f632e907f0313d18df4
Gabriel <132632103+bielaugustos@users.noreply.github.com>
2024-02-09 21:24:16 +0000

    style: project-card hover

--- app/components/pages/projects/projects-list/project-card/index.tsx
+++ app/components/pages/projects/projects-list/project-card/index.tsx
@@ -8,7 +8,7 @@ type ProjectCardProps = {
 export const ProjectCard = ({ project }: ProjectCardProps) => {
   const technologies = project.technologies.map((x) => x.name).join(', ')
   return (
-    <div className="rounded-lg h-[436px] flex flex-col bg-gray-800 overflow-hidden group transition-all border-2 border-gray-800 hover:border-emerald-500 opacity-70 hover:opacity-100">
+    <div className="rounded-lg h-[436px] flex flex-col bg-gray-800 overflow-hidden group transition-all border-2 border-gray-800 hover:border-amber-500 opacity-70 hover:opacity-100">
       <div className="w-full h-48 overflow-hidden">
         <Image
           width={380}
@@ -21,7 +21,7 @@ export const ProjectCard = ({ project }: ProjectCardProps) => {
       </div>
 
       <div className="flex-1 flex flex-col p-8">
-        <strong className="font-medium text-gray-50/90 group-hover:text-emerald-500 transition-all">
+        <strong className="font-medium text-gray-50/90 group-hover:text-amber-500 transition-all">
           {project.title}
         </strong>
         <p className="mt-2 text-gray-400 line-clamp-4">
